bbc0f1c72130c21fb6e571bb04a989d0d5cc3b6d
Clayton S <desertcow07@gmail.com>
2022-05-30 19:41:17 -0700

    All buttons/Text Input/Text Output to HTML and Javascript working

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,10 +14,51 @@ var liveCityHumidityEl = document.querySelector(".liveCityHumidity");
 var liveCityUVEl = document.querySelector(".liveCityUVindex");
 
 // ################# Forecast Card Day 1 #################
-var forecastCard1CityDateEL = document.querySelector(".forecastCard1-CityDate");
-var forecastCard1TempEL = document.querySelector(".day1ForeTemp");
-var forecastCard1WindEL = document.querySelector(".day1ForeWind");
-var forecastCard1HumidityEL = document.querySelector(".day1ForeHumidity");
+var forecastcard1CityDateEL = document.querySelector(".forecastcard1CityDate");
+var forecastcard1TempEL = document.querySelector(".day1ForeTemp");
+var forecastcard1WindEL = document.querySelector(".day1ForeWind");
+var forecastcard1HumidityEL = document.querySelector(".day1ForeHumidity");
+
+// ################# Forecast Card Day 2 #################
+var forecastcard2CityDateEL = document.querySelector(".forecastcard2CityDate");
+var forecastcard2TempEL = document.querySelector(".day2ForeTemp");
+var forecastcard2WindEL = document.querySelector(".day2ForeWind");
+var forecastcard2HumidityEL = document.querySelector(".day2ForeHumidity");
+
+// ################# Forecast Card Day 3 #################
+var forecastcard3CityDateEL = document.querySelector(".forecastcard3CityDate");
+var forecastcard3TempEL = document.querySelector(".day3ForeTemp");
+var forecastcard3WindEL = document.querySelector(".day3ForeWind");
+var forecastcard3HumidityEL = document.querySelector(".day3ForeHumidity");
+
+// ################# Forecast Card Day 4 #################
+var forecastcard4CityDateEL = document.querySelector(".forecastcard4CityDate");
+var forecastcard4TempEL = document.querySelector(".day4ForeTemp");
+var forecastcard4WindEL = document.querySelector(".day4ForeWind");
+var forecastcard4HumidityEL = document.querySelector(".day4ForeHumidity");
+
+// ################# Forecast Card Day 5 #################
+var forecastcard5CityDateEL = document.querySelector(".forecastcard5CityDate");
+var forecastcard5TempEL = document.querySelector(".day5ForeTemp");
+var forecastcard5WindEL = document.querySelector(".day5ForeWind");
+var forecastcard5HumidityEL = document.querySelector(".day5ForeHumidity");
+
+// ################# Search Text Box #################
+
+var searchBoxInput = document.querySelector(".searchBoxInput");
+
+// ################# Button Decleration #################
+
+var searchButton = document.querySelector(".searchBtn");
+
+var cityButtonn1 = document.querySelector(".cityBtn1");
+var cityButtonn2 = document.querySelector(".cityBtn2");
+var cityButtonn3 = document.querySelector(".cityBtn3");
+var cityButtonn4 = document.querySelector(".cityBtn4");
+var cityButtonn5 = document.querySelector(".cityBtn5");
+var cityButtonn6 = document.querySelector(".cityBtn6");
+var cityButtonn7 = document.querySelector(".cityBtn7");
+var cityButtonn8 = document.querySelector(".cityBtn8");
 
 
 let currentCityWeather = {
@@ -68,14 +109,86 @@ liveCityWindEl.textContent = " Wind: 15 mph";
 liveCityHumidityEl.textContent = "Humidity: 48%";
 liveCityUVEl.textContent = "UV Index: 0.45";
 
-forecastCard1CityDateEL.textContent = "Austin, TX  (6-2-2022)";
-forecastCard1TempEL.textContent = "Temperature: 78";
-forecastCard1WindEL.textContent = "Wind: 24 mph";
-forecastCard1HumidityEL.textContent = "Humidity: 69%";
+//forecastcard1CityDateEL.textContent = "Austin, TX  (6-2-2022)";
+forecastcard1TempEL.textContent = "Temperature: 78";
+forecastcard1WindEL.textContent = "Wind: 24 mph";
+forecastcard1HumidityEL.textContent = "Humidity: 69%";
+
+// forecastcard2CityDateEL.textContent = "Austin, TX  (6-3-2022)";
+forecastcard2TempEL.textContent = "Temperature: 56";
+forecastcard2WindEL.textContent = "Wind: 4 mph";
+forecastcard2HumidityEL.textContent = "Humidity: 39%";
+
+// forecastcard3CityDateEL.textContent = "Austin, TX  (6-4-2022)";
+forecastcard3TempEL.textContent = "Temperature: 66";
+forecastcard3WindEL.textContent = "Wind: 14 mph";
+forecastcard3HumidityEL.textContent = "Humidity: 83%";
+
+// forecastcard4CityDateEL.textContent = "Austin, TX  (6-5-2022)";
+forecastcard4TempEL.textContent = "Temperature: 74";
+forecastcard4WindEL.textContent = "Wind: 10 mph";
+forecastcard4HumidityEL.textContent = "Humidity: 59%";
+
+// forecastcard5CityDateEL.textContent = "Austin, TX  (6-6-2022)";
+forecastcard5TempEL.textContent = "Temperature: 68";
+forecastcard5WindEL.textContent = "Wind: 34 mph";
+forecastcard5HumidityEL.textContent = "Humidity: 49%";
 
 // // ############################## Button Listeners ##################################
 
+searchButton.addEventListener("click", function (clickEvent) {
 
+  console.log("Search Button Clicked! SEARCH = " + searchBoxInput.value);
+
+});
+
+cityButtonn1.addEventListener("click", function (clickEvent) {
+
+  console.log("City Button #1 Clicked!");
+
+});
+
+cityButtonn2.addEventListener("click", function (clickEvent) {
+
+  console.log("City Button #2 Clicked!");
+
+});
+
+cityButtonn3.addEventListener("click", function (clickEvent) {
+
+  console.log("City Button #3 Clicked!");
+
+});
+
+cityButtonn4.addEventListener("click", function (clickEvent) {
+
+  console.log("City Button #4 Clicked!");
+
+});
+
+cityButtonn5.addEventListener("click", function (clickEvent) {
+
+  console.log("City Button #5 Clicked!");
+
+});
+
+cityButtonn6.addEventListener("click", function (clickEvent) {
+
+  console.log("City Button #6 Clicked!");
+
+});
+
+cityButtonn7.addEventListener("click", function (clickEvent) {
+
+  console.log("City Button #7 Clicked!");
+
+});
+
+cityButtonn8.addEventListener("click", function (clickEvent) {
+
+  console.log("City Button #8 Clicked!");
+
+});
 
 
 // ############################ Mouse Over/Out Listeners ###############################
