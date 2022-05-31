b5e0d169da61b36c650471abdcc3aa818081d185
Clayton S <desertcow07@gmail.com>
2022-05-30 18:04:45 -0700

    Starting to connect HTML elements to Javascript variables | dinner commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,74 @@
 // 5-31-2022
 // ============================== Varriable Decleration ================================
 
+var currentTime = moment();
+
+// ################# Live City Card #################
+var liveCityDateEl = document.querySelector(".liveCityDate");
+var liveCityTempEl = document.querySelector(".liveCityTemp");
+var liveCityWindEl = document.querySelector(".liveCityWind");
+var liveCityHumidityEl = document.querySelector(".liveCityHumidity");
+var liveCityUVEl = document.querySelector(".liveCityUVindex");
+
+// ################# Forecast Card Day 1 #################
+var forecastCard1CityDateEL = document.querySelector(".forecastCard1-CityDate");
+var forecastCard1TempEL = document.querySelector(".day1ForeTemp");
+var forecastCard1WindEL = document.querySelector(".day1ForeWind");
+var forecastCard1HumidityEL = document.querySelector(".day1ForeHumidity");
 
 
+let currentCityWeather = {
 
+}
 
+var fiveDayForecast = [
+  {
+    "date": "6-1-2022",
+    "wxImgLink": "",
+    "temp": "70",
+    "wind": "15",
+    "humidity": "45",
+  },
+  {
+    "date": "6-2-2022",
+    "wxImgLink": "",
+    "temp": "70",
+    "wind": "15",
+    "humidity": "45",
+  },
+  {
+    "date": "6-3-2022",
+    "wxImgLink": "",
+    "temp": "70",
+    "wind": "15",
+    "humidity": "45",
+  },
+  {
+    "date": "6-4-2022",
+    "wxImgLink": "",
+    "temp": "70",
+    "wind": "15",
+    "humidity": "45",
+  },
+  {
+    "date": "6-5-2022",
+    "wxImgLink": "",
+    "temp": "70",
+    "wind": "15",
+    "humidity": "45",
+  }
+]
+
+liveCityDateEl.textContent = " Austin, Tx   6-1-2022";
+liveCityTempEl.textContent = " Temp: 70";
+liveCityWindEl.textContent = " Wind: 15 mph";
+liveCityHumidityEl.textContent = "Humidity: 48%";
+liveCityUVEl.textContent = "UV Index: 0.45";
+
+forecastCard1CityDateEL.textContent = "Austin, TX  (6-2-2022)";
+forecastCard1TempEL.textContent = "Temperature: 78";
+forecastCard1WindEL.textContent = "Wind: 24 mph";
+forecastCard1HumidityEL.textContent = "Humidity: 69%";
 
 // // ############################## Button Listeners ##################################
 
